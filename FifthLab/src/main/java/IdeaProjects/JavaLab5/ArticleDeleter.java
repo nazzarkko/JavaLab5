2f74b567ebfd489f56a8ce9860f4128617b3d7cb
nazzarkko <119264236+nazzarkko@users.noreply.github.com>
2023-04-30 23:48:51 +0300

    Fixed main method

--- FifthLab/src/main/java/IdeaProjects/JavaLab5/ArticleDeleter.java
+++ FifthLab/src/main/java/IdeaProjects/JavaLab5/ArticleDeleter.java
@@ -15,6 +15,14 @@ public class ArticleDeleter {
         System.out.println(result);
         return result;
     }
+     public static void main(String[] args) {
+        System.out.println("Enter a text: ");
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        System.out.println("Fixed text: ");
+        ArticleDeleter obj = new ArticleDeleter();
+        obj.deleteArticles(text);
+    }
 }
 
 
